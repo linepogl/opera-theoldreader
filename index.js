d574d768923777aa81619167f162904a85383ef4
Lazaros Inepologlou <li@tcpartners.fr>
2013-07-08 16:29:29 +0200

    Opera 15

---
+++ index.js
@@ -0,0 +1,85 @@
+var button = chrome.browserAction;
+
+button.onClicked.addListener(function(){
+	chrome.tabs.create({url:'http://theoldreader.com/posts/all'});
+});
+
+
+var Failure = function(error) {
+	button.setBadgeText({text:" ? "});
+	button.setIcon({path:"icon-inactive.png"});
+	button.setTitle({title:'The Old Reader\n\n' + error});
+	setTimeout(function() {
+	  Update();
+	}, 30000);
+};
+
+
+var Success = function(feedData) {
+	var s = 'The Old Reader\n';
+	var count = 0;
+	var i, folder;
+	for (i = 0; folder = feedData.feeds[i]; i++) {
+	  var k, feed;
+	  for (k = 0; feed = folder.feeds[k]; k++) {
+	    if (feed.unread_count) {
+	      s += '\n' + feed.title + ': ' + feed.unread_count;
+	      count += feed.unread_count;
+	    }
+	  }
+	}
+	for (i = 0; folder = feedData.following[i]; i++) {
+	  if (folder.unread_count) {
+	    count += folder.unread_count;
+	  }
+	}
+	
+	button.setBadgeText({text:(count==0?'':''+count)});
+	button.setIcon({path:"icon-active.png"});
+	button.setTitle({title:s});
+	setTimeout(function() {
+	  Update();
+	}, 60000);
+};
+
+
+var Update = function() {
+	var httpRequest = new XMLHttpRequest();
+	var requestTimeout = window.setTimeout(function() {
+	  httpRequest.abort();
+	  Failure('Error communicating with the server. Request timed out.');
+	}, 20000);
+	httpRequest.onerror = function() {
+	  Failure('Error communicating with the server.');
+	};
+	httpRequest.onreadystatechange = function() {
+	  if (httpRequest.readyState == 4) {
+	    window.clearTimeout(requestTimeout);
+	    if (httpRequest.status == 401) {
+	      Failure('You are not logged in.');
+	    } else if (httpRequest.status >= 400) {
+	      Failure('Error communicating with the server. HTTP status ' + httpRequest.status + '.');
+	    } else if (httpRequest.responseText) {
+	      var feedData = {
+	      };
+	      try {
+	        feedData = JSON.parse( httpRequest.responseText );
+	        Success(feedData);
+	      } catch (exception) {
+	        Failure('Error communicating with the server. Invalid server response.');
+	      }
+	    } else {
+	      Failure('Error communicating with the server. Invalid server response.');
+	    }
+	  }
+	};
+	try {
+	  httpRequest.open('GET', 'http://theoldreader.com/feeds/counts.json', true);
+	  httpRequest.send(null);
+	} catch (exception) {
+	  Failure('Error communicating with the server.');
+	}
+};
+
+Update();
+
